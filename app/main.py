e9271c281ecd06a9e1f1f31f57f11798238b8980
Lee Kang Martin Sutton <2403338@sit.singaporetech.edu.sg>
2025-07-02 16:30:20 +0800

    Added CORS support

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import routes
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your needs
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(routes.router)
 
